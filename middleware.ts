9418e2b36ec16371517a96dd29cac0fde7b6b7be
tahiee <niazi.tahirkhan15@gmail.com>
2024-09-03 17:01:54 +0500

    new udpates

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,9 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 const isPublicRoute = createRouteMatcher([
     '/',
     '/sign-in(.*)',
-    '/sign-up(.*)'])
+    '/sign-up(.*)',
+    '/api/webhook/clerk'
+])
 
 export default clerkMiddleware((auth, request) => {
     if (!isPublicRoute(request)) {
