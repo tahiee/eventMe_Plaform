0ce144e815e53b4bf404e3b1052e88df642d7734
tahiee <niazi.tahirkhan15@gmail.com>
2024-09-11 13:00:01 +0500

    update

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -1,6 +1,8 @@
 import { Button } from "@/components/ui/button";
 import Collection from "@/components/ui/shared/Collection";
 import { getEventsByUser } from "@/lib/actions/event.actions";
+import { getOrdersByUser } from "@/lib/actions/order.actions";
+import { IOrder } from "@/lib/database/models/order.model";
 import { auth } from "@clerk/nextjs/server";
 import Link from "next/link";
 import React from "react";
@@ -8,10 +10,12 @@ import React from "react";
 const ProfilePage = async () => {
   const { sessionClaims } = auth();
   const userId = sessionClaims?.userId as string;
-  console.log(userId, "userID");
 
   const organized = await getEventsByUser({ userId, page: 1 });
-  console.log(organized);
+
+  const orders = await getOrdersByUser({ userId, page: 1 });
+  const orderedEvents = orders?.data.map((order: IOrder) => order.event) || [];
+  console.log(orderedEvents);
 
   return (
     <>
@@ -26,10 +30,10 @@ const ProfilePage = async () => {
         </div>
       </section>
 
-      {/* <section className="wrapper my-8">
+      <section className="wrapper my-8">
         <Collection
-          data={events?.data}
-          emptyTitle="No Events Ticket Purchased yet" 
+          data={orderedEvents}
+          emptyTitle="No Events Ticket Purchased yet"
           emptyStateSubtext="No Worries - Plenty of exicting events of explore!"
           collectionType="My_Tickets"
           limit={3}
@@ -37,7 +41,7 @@ const ProfilePage = async () => {
           urlParamName="ordersPage"
           totalPages={2}
         />
-      </section> */}
+      </section>
 
       {/* Event Organizd */}
 
@@ -53,7 +57,7 @@ const ProfilePage = async () => {
       <section className="wrapper my-8">
         <Collection
           data={organized?.data}
-          emptyTitle="No Events have been Created yet" 
+          emptyTitle="No Events have been Created yet"
           emptyStateSubtext="Go Create some now"
           collectionType="Events_Organizer"
           limit={6}
